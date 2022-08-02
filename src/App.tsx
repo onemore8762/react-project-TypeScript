129371fb122801bab9157b8c33c28e3f115da36b
Denis <denis.churkin.afikei@mail.ru>
2022-08-02 21:47:33 +0300

    Add App, rating, pagetitle, star

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Accordion from "./components/Accordion/Accordion";
+import {Rating} from "./components/Rating/Rating";
 
 function App() {
-    return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo"/>
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-                text
-            </header>
 
-            <ul></ul>
-            <ul></ul>
+    return (
+        <div>
+            <PageTitle title={"This is APP component"}/>
+            <Accordion title={"Меню"}/>
+            <Rating value={0}/>
+            <Rating value={1}/>
+            <Rating value={2}/>
+            <Rating value={3}/>
+            <Rating value={4}/>
+            <Rating value={5}/>
         </div>
     );
+}
+
+function PageTitle(props: any) {
+    return <h1>{props.title}</h1>
 }
 
 export default App;
