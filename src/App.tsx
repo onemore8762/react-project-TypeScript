7bc4ce1e8f7f6044b82dc37743d4bde9be658b3a
Denis <denis.churkin.afikei@mail.ru>
2022-08-16 17:57:18 +0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,12 @@
 import React from 'react';
 import './App.css';
-import Accordion from "./components/Accordion/Accordion";
-import {Rating} from "./components/Rating/Rating";
 
 function App() {
-
-    return (
-        <div>
-            <PageTitle title={"This is APP component"}/>
-            <PageTitle title={"Text"}/>
-            <Accordion titleValue={"Меню"} collapsed={true}/>
-            <Accordion titleValue={"Меню-2"} collapsed={false}/>
-            <Rating value={0}/>
-            <Rating value={1}/>
-            <Rating value={2}/>
-            <Rating value={3}/>
-            <Rating value={4}/>
-            <Rating value={5}/>
-        </div>
-    );
-}
-
-type PageTitlePropsType = {
-    title: string;
-}
-
-function PageTitle(props: PageTitlePropsType) {
-    return <h1>{props.title}</h1>
+  return (
+    <div className="App">
+     Hello, samurai! Let's go!
+    </div>
+  );
 }
 
 export default App;
