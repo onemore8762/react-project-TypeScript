7bfe6b51c7362707ccd2330589ac89969a01ea5d
Denis <denis.churkin.afikei@mail.ru>
2022-08-20 21:27:27 +0300

    add Dialogs,News,Music,Settings

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,26 +1,27 @@
 import React from 'react'
 import s from './Dialogs.module.css'
+import {NavLink} from "react-router-dom";
 export const Dialogs = () => {
     return(
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
-                <div className={s.dialog}>
-                    Dimych
+                <div className={s.dialog + ' ' + s.active}>
+                    <NavLink to='/dialogs/1'>Dimych</NavLink>
                 </div>
                 <div className={s.dialog}>
-                    Andrey
+                    <NavLink to='/dialogs/2'>Andrey</NavLink>
                 </div>
                 <div className={s.dialog}>
-                    Sveta
+                    <NavLink to='/dialogs/3'>Sveta</NavLink>
                 </div>
                 <div className={s.dialog}>
-                    Sasha
+                    <NavLink to='/dialogs/4'>Sasha</NavLink>
                 </div>
                 <div className={s.dialog}>
-                    Viktor
+                    <NavLink to='/dialogs/5'>Viktor</NavLink>
                 </div>
                 <div className={s.dialog}>
-                    Denis
+                    <NavLink to='/dialogs/6'>Denis</NavLink>
                 </div>
             </div>
             <div className={s.messages}>
